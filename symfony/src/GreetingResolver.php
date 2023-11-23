85b9c9e5d9ff4d957e318620d89f6b7d350d7a52
Peter Majmesku <majmesku@publicplan.de>
2023-11-23 19:41:09 +0100

    Removed not needed makefile commands

---
+++ symfony/src/GreetingResolver.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use GraphQL\Type\Definition\ResolveInfo;
+use Overblog\GraphQLBundle\Definition\Resolver\QueryInterface;
+
+class GreetingResolver implements QueryInterface
+{
+    public function __invoke(ResolveInfo $info, mixed $value): mixed
+    {
+        $method = $info->fieldName;
+
+        if (!isset($info->variableValues['name'])) {
+            return $this->$method($value);
+        } else {
+            return $this->$method($value, $info->variableValues['name']);
+        }
+    }
+
+    public function resolve(): GreetingEntity
+    {
+        return new GreetingEntity('Hi,');
+    }
+
+    public function text(GreetingEntity $entity, string $name = ''): string
+    {
+        return $entity->greeting . ' ' . $name . '!';
+    }
+}
